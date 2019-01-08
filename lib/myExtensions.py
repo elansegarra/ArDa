186f9cad14a4bc64c1188bc9e7a4fa2f0eab75e7
Elan Segarra <elanster@gmail.com>
2019-01-08 14:43:47 -0600

    Added key press event to add lines to TextEditExt when enter is pressed (and minor tweaks to spaces and tabs).

--- lib/myExtensions.py
+++ lib/myExtensions.py
@@ -322,35 +322,43 @@ class mySortFilterProxy(QSortFilterProxyModel):
 
 # Customizing the QTextEdit widget so that it emits a finished editing signal
 class QTextEditExt(QTextEdit):
-    """
-    A TextEdit editor that sends editingFinished events
-    when the text was changed and focus is lost.
+	"""
+	A TextEdit editor that sends editingFinished events
+	when the text was changed and focus is lost.
 	This was found here: https://gist.github.com/hahastudio/4345418
-    """
-    editingFinished = pyqtSignal()
-    receivedFocus = pyqtSignal()
+	"""
+	editingFinished = pyqtSignal()
+	receivedFocus = pyqtSignal()
 
-    def __init__(self, parent):
-        super(QTextEditExt, self).__init__(parent)
-        self._changed = False
-        self.setTabChangesFocus( True )
-        self.textChanged.connect( self._handle_text_changed )
+	def __init__(self, parent):
+		super(QTextEditExt, self).__init__(parent)
+		self._changed = False
+		self.setTabChangesFocus( True )
+		self.textChanged.connect( self._handle_text_changed )
 
-    def focusInEvent(self, event):
-        super(QTextEditExt, self).focusInEvent( event )
-        self.receivedFocus.emit()
+	def focusInEvent(self, event):
+		super(QTextEditExt, self).focusInEvent( event )
+		self.receivedFocus.emit()
 
-    def focusOutEvent(self, event):
-        if self._changed:
-            self.editingFinished.emit()
-        super(QTextEditExt, self).focusOutEvent( event )
+	def focusOutEvent(self, event):
+		if self._changed:
+			self.editingFinished.emit()
+		super(QTextEditExt, self).focusOutEvent( event )
 
-    def _handle_text_changed(self):
-        self._changed = True
+	def _handle_text_changed(self):
+		self._changed = True
 
-    def setTextChanged(self, state=True):
-        self._changed = state
+	def setTextChanged(self, state=True):
+		self._changed = state
 
-    def setHtml(self, html):
-        QtGui.QTextEdit.setHtml(self, html)
-        self._changed = False
+	def setHtml(self, html):
+		QtGui.QTextEdit.setHtml(self, html)
+		self._changed = False
+
+	# This function stretches the height when enter is pressed
+	def keyPressEvent(self, event):
+		QTextEdit.keyPressEvent(self, event)
+		if event.key() == Qt.Key_Return:
+			# self.returnPressed.emit()
+			self.setFixedHeight(self.document().size().height()+10)
+			print("Return was pressed")
