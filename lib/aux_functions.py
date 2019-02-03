c49753ed97edf4861ea9feec90504b61c4ae3f0c
Elan Segarra <elanster@gmail.com>
2019-02-03 10:00:13 -0600

    Added title case with exceptions function.

--- lib/aux_functions.py
+++ lib/aux_functions.py
@@ -18,6 +18,17 @@ def timer(func):
         print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
         return value
     return wrapper_timer
+def title_except(sentence, exceptions = "normal"):
+	# This function capitalizes ever word except for those in exceptions
+	if exceptions == "normal":
+		exceptions = {'a', 'an', 'of', 'the', 'and', 'but', 'for', 'at', 'by',
+						'from', 'in', 'to'}
+	word_list = sentence.split(' ')       # re.split behaves as expected
+	final = [word_list[0].title()] #capitalize()]
+	for word in word_list[1:]:
+		word = word.lower()
+		final.append(word if word in exceptions else word.title()) #capitalize())
+	return " ".join(final)
 
 def addChildrenOf(parent_proj_id, project_df, ind_txt, proj_id_list, ignore_list = []):
     """
