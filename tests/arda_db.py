d4d47fe56d36e8c63c2d2104fb9e1f7b15ae8baf
Elan Segarra <elanster@gmail.com>
2022-04-17 13:14:48 -0400

    Created base arda_db class with subclasses

---
+++ tests/arda_db.py
@@ -0,0 +1,87 @@
+# This define a class which allows for interacting with an ArDa database object
+
+import sqlite3
+import pandas as pd
+from os.path import exists, isfile, join
+from os import makedirs, listdir
+
+class ArDa_DB:
+    doc_vars = ['doc_id', 'title', 'year']
+    proj_vars = ['proj_id', 'title', 'parent', 'children']
+
+    def __init__(self, db_type):
+        pass
+
+    def make_new_db(self, db_path):
+        raise NotImplementedError
+
+    def open_db(self, db_path):
+        # Check if path is valid
+        if (not exists(db_path)):
+            raise FileNotFoundError
+        self.db_path = db_path
+
+    def add_doc_record(self, doc_dict):
+        raise NotImplementedError
+
+    def delete_doc_record(self, doc_id):
+        raise NotImplementedError
+
+    def update_doc_record(self, doc_dict):
+        raise NotImplementedError
+
+    def get_doc_record(self, doc_id):
+        raise NotImplementedError
+
+    def get_next_doc_id(self):
+        raise NotImplementedError
+
+class ArDa_DB_SQL(ArDa_DB):
+    def __init__(self):
+        self.db_type = "sqllite"
+
+    def make_new_db(self, db_path):
+        # Check that path is valid and the file does not exist currently
+        if (exists(db_path)):
+            raise FileExistsError
+
+        # Create the file and connect to it
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        # Create main document table
+        c.execute("CREATE TABLE 'Documents' ( `doc_id` INTEGER NOT NULL, `doc_type` TEXT, `title` TEXT, PRIMARY KEY(`doc_id`) )")
+        
+        conn.close()
+        self.db_path = db_path
+
+
+class ArDa_DB_Obsid(ArDa_DB):
+    def __init__(self):
+        self.db_type = "obsidian"
+
+    def make_new_db(self, db_path):
+        # Make the folder if it doesn't exist (and check if empty if it does exist)
+        if exists(db_path):
+            num_files = len([name for name in listdir(db_path) if isfile(join(db_path, name))])
+            print(f"Found {num_files} files in {db_path}.")
+            if num_files > 0:
+                raise FileExistsError
+        else:
+            makedirs(db_path)
+        self.db_path = db_path
+
+class ArDa_DB_Bib(ArDa_DB):
+    def __init__(self):
+        self.db_type = "bibtex"
+
+    def make_new_db(self, db_path):
+        # Make the folder if it doesn't exist (and check if empty if it does exist)
+        if exists(db_path):
+            num_files = len([name for name in listdir(db_path) if isfile(join(db_path, name))])
+            print(f"Found {num_files} files in {db_path}.")
+            if num_files > 0:
+                raise FileExistsError
+        else:
+            makedirs(db_path)
+        self.db_path = db_path
