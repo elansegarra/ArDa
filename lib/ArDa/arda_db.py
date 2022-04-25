d74ca6045f0f529c24565a854002efd05e7bf183
Elan Segarra <elanster@gmail.com>
2022-04-25 00:34:47 -0400

    Implemented parsing of single obsid file

--- lib/ArDa/arda_db.py
+++ lib/ArDa/arda_db.py
@@ -62,6 +62,7 @@ class ArDa_DB_SQL(ArDa_DB):
 class ArDa_DB_Obsid(ArDa_DB):
     def __init__(self):
         self.db_type = "obsidian"
+        self.db_path = None
 
     def make_new_db(self, db_path):
         # Make the folder if it doesn't exist (and check if empty if it does exist)
@@ -76,7 +77,48 @@ class ArDa_DB_Obsid(ArDa_DB):
     
     def parse_obsid_file(self, file_path):
         # Reads an obsidian file and returns a dictionary of the contents
-        raise NotImplementedError
+
+        # Check if file exists (either as is or relative to db_path)
+        if (self.db_path is not None) and (exists(self.db_path+"/"+file_path)):
+            file_path = self.db_path+"/"+file_path
+        elif (exists(file_path)):   pass
+        else:                       raise FileNotFoundError
+
+        # Read the file
+        file_contents = {}
+        with open(file_path) as f:
+            lines = f.readlines()
+
+        # Parse the yml
+        assert lines[0].strip() == "---"
+        for i in range(1, len(lines)):
+            if lines[i].strip() == "---":
+                yml_line_end = i
+                break
+            line_vals = lines[i].split(":")
+            file_contents[line_vals[0].strip()] = line_vals[1].strip()
+            if len(line_vals) > 2:
+                raise FormatError
+
+        # Parse the rest of the file (break up by headings)
+        h_num, h_title, h_body = 0, "No heading", ""
+        print(yml_line_end)
+        for i in range(yml_line_end+1, len(lines)):
+            line = lines[i]
+            if (len(line)>0) and (line[0] == "#"):
+                file_contents["h"+str(h_num)] = {"title": h_title, "body": h_body}
+                h_num += 1
+                h_title = line[1:]
+                h_body = ""
+            else:
+                h_body += line
+        # Storing last group 
+        file_contents["h"+str(h_num)] = {"title": h_title, "body": h_body}
+
+        return file_contents
+
+
+
 
 class ArDa_DB_Bib(ArDa_DB):
     def __init__(self):
